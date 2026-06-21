8b493a524246cb78397ab6c9396a014c4df6ae4e
Aleksey Bogomolov <bogomolov2004alex@gmail.com>
2026-06-21 16:26:52 +0300

    datasets: Restore dataset pagination

--- src/apps/datasets/api/v1/views.py
+++ src/apps/datasets/api/v1/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from apps.datasets.services import DatasetService
 
@@ -7,37 +6,13 @@ from .serializers import DatasetDetailedSerializer
 
 
 class DatasetsViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    Datasets API endpoint that allows datasets to be viewed only.
-    """
+    """Read-only dataset endpoint."""
 
     serializer_class = DatasetDetailedSerializer
 
     @property
-    def _dataset_service(self):
+    def _dataset_service(self) -> DatasetService:
         return DatasetService()
 
     def get_queryset(self):
         return self._dataset_service.get_all_detailed()
-
-    def list(self, request):
-        """
-        Get all datasets with detailed information about each dataset
-        """
-        datasets = self.get_queryset()
-        serializer = self.get_serializer(datasets, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        """
-        Get detailed information about a specific dataset
-        """
-        if not pk:
-            return Response("Provide primary key", status=status.HTTP_400_BAD_REQUEST)
-
-        dataset = self._dataset_service.get_one_detailed(id=pk)
-        if not dataset:
-            return Response("Dataset does not exist", status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.get_serializer(dataset)
-        return Response(serializer.data)
--- src/apps/datasets/services.py
+++ src/apps/datasets/services.py
@@ -6,26 +6,12 @@ class DatasetService:
     Business logic class for Dataset model.
     """
 
-    def get_one_detailed(self, id):
-        """
-        Get specific dataset with all known information about it.
-        """
-        try:
-            return (
-                Dataset.objects.select_related("anatomical_area")
-                .prefetch_related("modalities", "ml_tasks", "tags")
-                .get(id=id)
-            )
-        except Dataset.DoesNotExist:
-            # TODO: Handle/log error
-            return None
-
     def get_all_detailed(self):
-        """
-        Get all datasets with all known information about each one of them.
-        """
+        """Get all datasets with their related information."""
+
         return (
-            Dataset.objects.all()
+            Dataset.objects
             .select_related("anatomical_area")
             .prefetch_related("modalities", "ml_tasks", "tags")
+            .order_by("-created_at", "-id")
         )
